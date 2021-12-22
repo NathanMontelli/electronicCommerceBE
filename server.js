6677227480f9b79c58c1c7ba2b878dc506883e9d
Nathan Montelli <nathanmontelli@gmail.com>
2021-12-21 22:40:14 -0800

    added function to turn on database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {Category, Product, ProductTag, Tag} = require('./models')
-// import sequelize connection
 
+// import sequelize connection
 const app = express()
 
 app.use(express.json())
@@ -10,5 +10,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+async function init () {
+  await require('./config/connection.js').sync()
+  app.listen(process.env.PORT || 3000)
+}
 
+init()
